ab668b6fcb50d5eb9049033e2cfdd248d4697574
rzamarefat <rzamarefat@gmail.com>
2023-11-19 14:57:21 +0330

    new conept

---
+++ boolalpha.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool isOn = true;
+
+    cout << "The flag is: " << isOn << endl;
+    cout << "The flag is: " << boolalpha << isOn << endl;
+    
+    return 0;
+}
